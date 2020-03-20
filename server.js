eaccc0a386b7c627cf7edcc4744727c113d8193b
Karl-Erik Kald <karl80038@gmail.com>
2020-03-20 16:11:54 +0200

    Changes in app.listen in Server.JS file, add .gitignore

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.use(function(req, res, next){
     });
 });
 
-app.listen(5000, ()=> {
-    console.log("Server is running on port 3000.");
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server has started");
 })
